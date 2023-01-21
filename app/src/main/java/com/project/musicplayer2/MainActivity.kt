b0568c9c70313609fd9747b810f3000765ad7f40
Abinash <abinashdash6000@gmail.com>
2023-01-21 12:48:53 +0530

    Update

--- app/src/main/java/com/project/musicplayer2/MainActivity.kt
+++ app/src/main/java/com/project/musicplayer2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.project.musicplayer2
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.MenuItem
@@ -21,11 +22,23 @@ class MainActivity : AppCompatActivity() {
     private lateinit var musicAdapter: MusicAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initializeLayout()
+        setTheme(R.style.coolPinkNav)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // for nav drawer
+        toggle = ActionBarDrawerToggle(this, binding.root, R.string.open, R.string.close)
+        binding.root.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        if(requestRuntimePermission())
+            initializeLayout()
 
         binding.apply {
             shuffleBtn.setOnClickListener {
                 val intent = Intent(this@MainActivity, PlayerActivity::class.java)
+                intent.putExtra("index", 0)
+                intent.putExtra("class", "MainActivity")
                 startActivity(intent)
             }
             favouriteBtn.setOnClickListener {
@@ -48,10 +61,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
     // For requesting permission
-    private fun requestRuntimePermission() {
+    private fun requestRuntimePermission(): Boolean {
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 13)
+            return false
         }
+        return true
     }
 
     // Action when the permission is granted or not granted
@@ -60,6 +75,7 @@ class MainActivity : AppCompatActivity() {
         if(requestCode == 13) {
             if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                initializeLayout()
             } else {
                 ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 13)
             }
@@ -72,16 +88,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initializeLayout() {
-        requestRuntimePermission()
-        setTheme(R.style.coolPinkNav)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        // for nav drawer
-        toggle = ActionBarDrawerToggle(this, binding.root, R.string.open, R.string.close)
-        binding.root.addDrawerListener(toggle)
-        toggle.syncState()
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         MusicListMA = getAllAudio()
         binding.musicRV.setHasFixedSize(true)
         binding.musicRV.setItemViewCacheSize(30)
@@ -95,7 +102,7 @@ class MainActivity : AppCompatActivity() {
     private fun getAllAudio(): ArrayList<Music> {
         val tempList = ArrayList<Music>()
         val selection = MediaStore.Audio.Media.IS_MUSIC + " != 0"
-        val projection = arrayOf(MediaStore.Audio.Media._ID, MediaStore.Audio.Media.TITLE, MediaStore.Audio.Media.ALBUM, MediaStore.Audio.Media.ARTIST, MediaStore.Audio.Media.DURATION, MediaStore.Audio.Media.DATE_ADDED, MediaStore.Audio.Media.DATA)
+        val projection = arrayOf(MediaStore.Audio.Media._ID, MediaStore.Audio.Media.TITLE, MediaStore.Audio.Media.ALBUM, MediaStore.Audio.Media.ARTIST, MediaStore.Audio.Media.DURATION, MediaStore.Audio.Media.DATE_ADDED, MediaStore.Audio.Media.DATA, MediaStore.Audio.Media.ALBUM_ID)
         val cursor = this.contentResolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, selection, null, MediaStore.Audio.Media.DATE_ADDED+" DESC", null)
         cursor?.let {
             if(it.moveToFirst())
@@ -106,7 +113,10 @@ class MainActivity : AppCompatActivity() {
                     val artistC = it.getString(it.getColumnIndex(MediaStore.Audio.Media.ARTIST))
                     val pathC = it.getString(it.getColumnIndex(MediaStore.Audio.Media.DATA))
                     val durationC = it.getLong(it.getColumnIndex(MediaStore.Audio.Media.DURATION))
-                    val music = Music(id = idC, title = titleC, album = albumC, artist = artistC, duration = durationC, path = pathC)
+                    val albumIdC = it.getLong(it.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID)).toString()
+                    val uri = Uri.parse("content://media/external/audio/albumart")
+                    val artUriC = Uri.withAppendedPath(uri, albumIdC).toString()
+                    val music = Music(id = idC, title = titleC, album = albumC, artist = artistC, duration = durationC, path = pathC, artUri = artUriC)
                     val file = File(music.path)
                     if (file.exists())
                         tempList.add(music)
--- app/src/main/java/com/project/musicplayer2/PlayerActivity.kt
+++ app/src/main/java/com/project/musicplayer2/PlayerActivity.kt
@@ -1,7 +1,11 @@
 package com.project.musicplayer2
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.project.musicplayer2.databinding.ActivityPlayerBinding
 
 class PlayerActivity : AppCompatActivity() {
@@ -11,5 +15,84 @@ class PlayerActivity : AppCompatActivity() {
         setTheme(R.style.coolPink)
         binding = ActivityPlayerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initializeLayout()
+        binding.playPauseBtnPA.setOnClickListener {
+            if(isPlaying) pauseMusic()
+            else playMusic()
+        }
+        binding.previousBtnPA.setOnClickListener {
+            prevNextSong(increment = false)
+        }
+        binding.nextBtnPA.setOnClickListener {
+            prevNextSong(increment = true)
+        }
+    }
+    private fun setLayout() {
+        Glide.with(this).load(musicListPA[songPosition].artUri)
+            .apply(RequestOptions().placeholder(R.drawable.splash_screen).centerCrop())
+            .into(binding.songImgPA)
+        binding.songNamePA.text = musicListPA[songPosition].title
+    }
+    private fun createMediaPlayer() {
+        try {
+            if (mediaPlayer == null) mediaPlayer = MediaPlayer()
+            mediaPlayer!!.reset()
+            mediaPlayer!!.setDataSource(musicListPA[songPosition].path)
+            mediaPlayer!!.prepare()
+            mediaPlayer!!.start()
+            isPlaying = true
+            binding.playPauseBtnPA.setIconResource(R.drawable.pause_icon)
+        } catch (e: Exception) {
+            Log.d("someError", "Some error occurred")
+            return
+        }
+    }
+    private fun initializeLayout() {
+        songPosition = intent.getIntExtra("index", 0)
+        when(intent.getStringExtra("class")) {
+            "MusicAdapter" -> {
+                musicListPA = ArrayList()
+                musicListPA.addAll(MainActivity.MusicListMA)
+                setLayout()
+                createMediaPlayer()
+            }
+            "MainActivity" -> {
+                musicListPA = ArrayList()
+                musicListPA.addAll(MainActivity.MusicListMA)
+                musicListPA.shuffle()
+                setLayout()
+                createMediaPlayer()
+            }
+        }
+    }
+    private fun playMusic() {
+        binding.playPauseBtnPA.setIconResource(R.drawable.pause_icon)
+        isPlaying = true
+        mediaPlayer!!.start()
+    }
+    private fun pauseMusic() {
+        binding.playPauseBtnPA.setIconResource(R.drawable.play_icon)
+        isPlaying = false
+        mediaPlayer!!.pause()
+    }
+    private fun prevNextSong(increment: Boolean) {
+        setSongPosition(increment)
+        setLayout()
+        createMediaPlayer()
+    }
+    private fun setSongPosition(increment: Boolean) {
+        if (increment) {
+            if (musicListPA.size-1 == songPosition) songPosition = 0
+            else songPosition++
+        } else {
+            if (songPosition == 0) songPosition = musicListPA.size-1
+            else songPosition--
+        }
+    }
+    companion object {
+        lateinit var  musicListPA: ArrayList<Music>
+        var songPosition: Int = 0
+        var mediaPlayer: MediaPlayer? = null
+        var isPlaying = false
     }
 }
